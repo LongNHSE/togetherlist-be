b1f576d438c180e75c3852269c9a2c27404aa0ca
Nguyen Thoai <itnat1503@gmail.com>
2024-05-21 21:51:58 +0700

    handle api user and password [Nguyen Thoai]

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -4,12 +4,16 @@ import { UserController } from './user.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, userSchema } from './schema/user.schema';
 import { ImageModule } from '../image/image.module';
+import { JwtModule } from '@nestjs/jwt';
+import { OTP, otpSchema } from '../otp/schema/otp.schema';
 
 @Module({
   controllers: [UserController],
   imports: [
     ImageModule,
     MongooseModule.forFeature([{ name: User.name, schema: userSchema }]),
+    MongooseModule.forFeature([{ name: OTP.name, schema: otpSchema }]),
+    JwtModule.register({})
   ],
   providers: [UserService],
   exports: [UserService],
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,12 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './schema/user.schema';
-import { Model, ObjectId } from 'mongoose';
+import { Model } from 'mongoose';
+import { JwtService } from '@nestjs/jwt';
+import { UpdatePasswordDto } from './dto/update-password.dto';
+import * as bcrypt from 'bcrypt';
+import { ResetPasswordDto } from './dto/reset-password.dto';
+import { OTP } from '../otp/schema/otp.schema';
 
 @Injectable()
 export class UserService {
+  constructor(
+    @InjectModel(User.name) private userModel: Model<User>,
+    @InjectModel(OTP.name) private otpModel: Model<OTP>,
+    private readonly jwtService: JwtService
+  ) {}
+
   updateImage(id: string, urlResult: string) {
     return this.userModel.findByIdAndUpdate(
       id,
@@ -16,16 +27,17 @@ export class UserService {
       },
     );
   }
-  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  async createUser(createUserDto: CreateUserDto) {
+    return this.userModel.create(createUserDto);
   }
+
   checkExistedEmail(email: string) {
     return this.userModel.findOne({
       email: email,
     });
   }
+
   async findAll() {
     return this.userModel.find();
   }
@@ -40,7 +52,50 @@ export class UserService {
     });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async deleteUser(id: string) {
+    return this.userModel.findByIdAndDelete(id);
+  }
+
+  async findById(id: string) {
+    return this.userModel.findById(id);
+  }
+
+  async getProfileDetailByToken(jwt: string) {
+    const token = jwt.replace('Bearer ', '');
+    const decodedToken = await this.jwtService.decode(token);
+    const account = await this.findById(decodedToken?.userId);
+
+    return account;
+  }
+
+  async updateUserPassword(updatePasswordDto: UpdatePasswordDto, jwt: string) {
+    const { oldPassword, newPassword } = updatePasswordDto;
+    const token = jwt.replace('Bearer ', '');
+    const decodedToken = await this.jwtService.decode(token);
+    const account = await this.findById(decodedToken?.userId);
+
+    if (!account) {
+      throw new NotFoundException('Account not found');
+    }
+
+    const match = await bcrypt.compareSync(oldPassword, account.password);
+    if (!match) {
+      throw new HttpException('Password is not correct', 400);
+    }
+
+    const hash = await bcrypt.hash(newPassword, 10);
+
+    await this.userModel.findByIdAndUpdate(account._id, { password: hash });
+  }
+
+  async resetUserPassword(resetUserPassword: ResetPasswordDto) {
+    const { email: mail, newPassword, otp: OTP } = resetUserPassword;
+    const receivedOtp = await this.otpModel.findOne({ mail, OTP });
+
+    if (!receivedOtp || receivedOtp.OTP !== OTP) {
+      throw new HttpException('Invalid OTP', 400);
+    }
+    const hash = await bcrypt.hash(newPassword, 10);
+    await this.userModel.findOneAndUpdate({ email: mail }, { password: hash });
   }
 }
